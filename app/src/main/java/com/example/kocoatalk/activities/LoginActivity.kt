960063d249bb05c4f5df976d6691988a60ba4eb2
Joon Ho Bae <ppow1123@naver.com>
2024-08-17 16:51:59 +0900

    240817

--- app/src/main/java/com/example/kocoatalk/activities/LoginActivity.kt
+++ app/src/main/java/com/example/kocoatalk/activities/LoginActivity.kt
@@ -33,17 +33,21 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         initializeViews()
         setOnClickListeners()
     }
 
     private fun initializeViews() {
-        val clientId = getString(R.string.social_login_info_naver_client_id)
-        val clientSecret = getString(R.string.social_login_info_naver_client_secret)
-        val clientName = getString(R.string.social_login_info_naver_client_name)
         prefUtil = PreferenceUtil(this)
         pref = prefUtil.getPreferences("pref_logedin_user")
-        NaverIdLoginSDK.initialize(this, clientId, clientSecret, clientName)
+
+        NaverIdLoginSDK.initialize(
+            this,
+            getString(R.string.social_login_info_naver_client_id),
+            getString(R.string.social_login_info_naver_client_secret),
+            getString(R.string.social_login_info_naver_client_name)
+        )
     }
 
     private fun setOnClickListeners() {
@@ -68,11 +72,7 @@ class LoginActivity : AppCompatActivity() {
         val email = binding.edtEmailLogin.text.toString()
         val password = binding.edtPwLogin.text.toString()
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl(LoginInterface.Login_Url)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
+        val retrofit = createRetrofitInstance(LoginInterface.Login_Url)
         val loginService = retrofit.create(LoginInterface::class.java)
         val userLogin = User_login(email, password)
 
@@ -81,13 +81,13 @@ class LoginActivity : AppCompatActivity() {
                 if (response.isSuccessful) {
                     handleLoginResponse(response.body()?.string(), email)
                 } else {
-                    Log.d("LoginDebug", "Response error: ${response.errorBody()?.string()}")
-                    Toast.makeText(applicationContext, "로그인 에러: 가입되지 않은 이메일입니다.", Toast.LENGTH_SHORT).show()
+                    showToast("로그인 에러: 가입되지 않은 이메일입니다.")
+                    logError("Response error: ${response.errorBody()?.string()}")
                 }
             }
 
             override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                Toast.makeText(applicationContext, "네트워크 오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
+                showToast("네트워크 오류가 발생했습니다.")
             }
         })
     }
@@ -96,71 +96,58 @@ class LoginActivity : AppCompatActivity() {
         responseBody?.let {
             val jsonObject = JSONObject(it)
             val message = jsonObject.getString("message")
-            val id = JSONObject(it).getString("userid")
-            val name = JSONObject(it).getString("username")
+            val userId = jsonObject.getString("userid")
+            val username = jsonObject.getString("username")
+
             when (message) {
                 "Login successful" -> {
-                    prefUtil.setBoolean(pref, "logged in", true)
-                    prefUtil.setString(pref, "loginmethod", "email")
-                    prefUtil.setString(pref, "userid", id)
-                    val name = JSONObject(it).getString("username")
-                    prefUtil.setString(pref, "username", name)
-                    Toast.makeText(applicationContext, "$name 님 환영합니다.\n userid: $id", Toast.LENGTH_SHORT).show()
-                    startActivity(Intent(this, MainActivity::class.java))
-                    finish()
+                    saveLoginData("email", userId, username)
+                    showToast("$username 님 환영합니다.\nuserid: $userId")
+                    navigateToMainActivity()
                 }
-                "Incorrect password" -> {
-                    Toast.makeText(applicationContext, "비밀번호가 일치하지 않습니다", Toast.LENGTH_SHORT).show()
-                }
-                else -> {
-                    Toast.makeText(applicationContext, "가입되지 않은 이메일입니다.", Toast.LENGTH_SHORT).show()
-                }
+                "Incorrect password" -> showToast("비밀번호가 일치하지 않습니다")
+                else -> showToast("가입되지 않은 이메일입니다.")
             }
         }
     }
 
     private fun performNaverLogin() {
-        val oAuthLoginCallback = object : OAuthLoginCallback {
+        NaverIdLoginSDK.authenticate(this, object : OAuthLoginCallback {
             override fun onSuccess() {
                 NidOAuthLogin().callProfileApi(object : NidProfileCallback<NidProfileResponse> {
                     override fun onSuccess(result: NidProfileResponse) {
-                        val name = result.profile?.name.toString()
-                        val id = result.profile?.id.toString()
-                        Log.d("NaverLogin", "Profile: ${result.profile}")
-
-                        registerUser(name, id)
+                        result.profile?.let {
+                            val name = it.name.toString()
+                            val id = it.id.toString()
+                            logDebug("Profile: ${it}")
+                            registerUser(name, id)
+                        }
                     }
 
                     override fun onError(errorCode: Int, message: String) {
-                        Log.e("NaverLogin", "Error Code: $errorCode, Message: $message")
+                        logError("NaverLogin Error Code: $errorCode, Message: $message")
                     }
 
                     override fun onFailure(httpStatus: Int, message: String) {
-                        Log.e("NaverLogin", "Failure: HttpStatus: $httpStatus, Message: $message")
+                        logError("NaverLogin Failure: HttpStatus: $httpStatus, Message: $message")
                     }
                 })
             }
 
             override fun onError(errorCode: Int, message: String) {
-                Log.e("NaverLogin", "Error Code: $errorCode, Message: $message, Token: ${NaverIdLoginSDK.getAccessToken()}")
+                logError("NaverLogin Error Code: $errorCode, Message: $message")
             }
 
             override fun onFailure(httpStatus: Int, message: String) {
-                Log.e("NaverLogin", "Failure: HttpStatus: $httpStatus, Message: $message")
+                logError("NaverLogin Failure: HttpStatus: $httpStatus, Message: $message")
             }
-        }
-
-        NaverIdLoginSDK.authenticate(this, oAuthLoginCallback)
+        })
     }
 
-    private fun registerUser(navername: String, naverID: String) {
-        val retrofit = Retrofit.Builder()
-            .baseUrl(SignupInterface.Register_Url)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
+    private fun registerUser(name: String, naverID: String) {
+        val retrofit = createRetrofitInstance(SignupInterface.Register_Url)
         val signupService = retrofit.create(SignupInterface::class.java)
-        val user = UserSignup(null, null, navername, "naver", naverID)
+        val user = UserSignup(null, null, name, "naver", naverID)
 
         signupService.registerUser(user).enqueue(object : Callback<ResponseBody> {
             override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
@@ -170,31 +157,60 @@ class LoginActivity : AppCompatActivity() {
             }
 
             override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                Toast.makeText(applicationContext, "네트워크 오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
+                showToast("네트워크 오류가 발생했습니다.")
             }
         })
     }
 
     private fun handleRegisterResponse(responseBody: String?) {
         responseBody?.let {
-            val message = JSONObject(it).getString("message")
-            val id = JSONObject(it).getString("userid")
-           val name = JSONObject(it).getString("username")
+            val jsonObject = JSONObject(it)
+            val message = jsonObject.getString("message")
+            val userId = jsonObject.getString("userid")
+            val username = jsonObject.getString("username")
+
             when (message) {
                 "User registered successfully" -> {
-                    Toast.makeText(applicationContext, "가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+                    showToast("가입이 완료되었습니다.")
                 }
                 "User already exists" -> {
-                    prefUtil.setBoolean(pref, "logged in", true)
-                    prefUtil.setString(pref, "loginmethod", "naver")
-                    prefUtil.setString(pref, "userid", id)
-                    prefUtil.setString(pref, "username", name)
-
-                    startActivity(Intent(this, MainActivity::class.java))
-                    finish()
+                    saveLoginData("naver", userId, username)
+                    navigateToMainActivity()
                 }
             }
-
         }
+    }
+
+    private fun saveLoginData(loginMethod: String, userId: String, username: String) {
+        prefUtil.apply {
+            setBoolean(pref, "logged in", true)
+            setString(pref, "loginmethod", loginMethod)
+            setString(pref, "userid", userId)
+            setString(pref, "username", username)
+        }
+    }
+
+    private fun navigateToMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun logError(message: String) {
+        Log.e("LoginActivity", message)
+    }
+
+    private fun logDebug(message: String) {
+        Log.d("LoginActivity", message)
+    }
+
+    private fun createRetrofitInstance(baseUrl: String): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 }
